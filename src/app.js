7a453b7dd1125b4911bbd17cffaf6dda7e38157b
Stephon Griffen <56489291+stephon305@users.noreply.github.com>
2023-08-28 00:40:54 +0000

    finale

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateDomain(){
+  let pronoun = ['the','our'];
+  let adj = ['great', 'big' ];
+  let noun = ['jogger','racoon'];
+  let cite = [".com", ".edu",".net"]
+  for(let i=0; i < pronoun.length;i++)
+  for(let x=0; x < adj.length;x++)
+  for(let y=0; y < noun.length;y++)
+  for(let e=0; e < cite.length;e++)
+console.log(pronoun[i] +adj[x] +noun[y] +cite[e])
+}
+console.log(generateDomain());
